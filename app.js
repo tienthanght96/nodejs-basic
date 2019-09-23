1aa8216397cea0b574983b830029bbc2988ef38e
tienthanght96 <trantienthanght96@gmail.com>
2019-09-23 23:19:22 +0700

    Update error handling middleware

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const tourRouter = require('./routers/tourRoutes');
 const userRouter = require('./routers/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 app.use(express.static(`${__dirname}/public`));
@@ -18,5 +21,13 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+// Handle incorrect request
+app.all('*', (req, res, next) => {
+  const message = `Can't find ${req.originalUrl} on this server!`;
+  const statusCode = 404;
+  next(new AppError(message, statusCode));
+});
+// Handle middleware error with express
+app.use(globalErrorHandler);
 
 module.exports = app;
